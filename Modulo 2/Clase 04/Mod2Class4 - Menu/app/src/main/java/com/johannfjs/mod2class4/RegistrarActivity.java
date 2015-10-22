6366e59e11d29df425650862d1af326c5bcecafa
johannfjs <johannfjs@gmail.com>
2015-10-21 20:10:57 -0500

    clase 04 - menu

---
+++ Modulo 2/Clase 04/Mod2Class4 - Menu/app/src/main/java/com/johannfjs/mod2class4/RegistrarActivity.java
@@ -0,0 +1,14 @@
+package com.johannfjs.mod2class4;
+
+import android.os.Bundle;
+
+import com.johannfjs.sliding.BaseActivity;
+
+public class RegistrarActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar);
+    }
+}
